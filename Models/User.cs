2363997e56a7aec2d90c72942e54dc63eb81f6ac
Wesley254 <wesleykirui2021@gmail.com>
2023-08-26 12:36:31 +0300

    dir restructure

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectMangementSystem.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; } = false;
+
+        public ICollection<Task> AssignedTasks { get; set; }
+    }
+}
